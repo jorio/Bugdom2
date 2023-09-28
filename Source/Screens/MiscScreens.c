a5f8004d1cfe831bfd76204d5ebc188b108ee57f
Iliyas Jorio <iliyas@jor.io>
2023-09-29 00:13:15 +0200

    Level intro non-English subtitles

--- Source/Screens/MiscScreens.c
+++ Source/Screens/MiscScreens.c
@@ -1,7 +1,8 @@
 /****************************/
 /*   	MISCSCREENS.C	    */
-/* (c)2002 Pangea Software  */
 /* By Brian Greenstone      */
+/* (c)2002 Pangea Software  */
+/* (c)2023 Iliyas Jorio     */
 /****************************/
 
 
@@ -308,6 +309,22 @@ HICommand 			command;
 #pragma mark -
 
 
+/********************* LEVEL INTRO SUBTITLE ***********************/
+
+static void DrawLevelIntroSubtitle(ObjNode* objNode)
+{
+	objNode->Timer += gFramesPerSecondFrac;
+
+	if (objNode->Timer < 0)
+		return;
+
+	OGL_PushState();
+	SetInfobarSpriteState();
+	gGlobalTransparency = SDL_min(1, 2 * objNode->Timer);
+	GameFont_DrawString(Localize(STR_LEVEL1 + gLevelNum), 320, 400, 0.6f, kTextMeshAlignCenter);
+	OGL_PopState();
+}
+
 /*********************** DO LEVEL INTRO ***************************/
 
 void DoLevelIntro(void)
@@ -320,6 +337,30 @@ void DoLevelIntro(void)
 		gAnaglyphFocallength	= 200.0f * gAnaglyphScaleFactor;	// set camera info
 		gAnaglyphEyeSeparation 	= 25.0f * gAnaglyphScaleFactor;
 	}
+
+			/* SHOW NON-ENGLISH SUBTITLE */
+
+	if (gGamePrefs.language != LANGUAGE_ENGLISH
+		&& Localize(STR_LEVEL1 + gLevelNum)[0] != '-')		// don't show untranslated subtitles
+	{
+		static const float timers[] =
+		{
+			[LEVEL_NUM_GNOMEGARDEN]	= 5,
+			[LEVEL_NUM_SIDEWALK]	= 12,
+			[LEVEL_NUM_FIDO]		= 3,
+			[LEVEL_NUM_PLUMBING]	= 7,
+			[LEVEL_NUM_PLAYROOM]	= 5,
+			[LEVEL_NUM_CLOSET]		= 6.5f,
+			[LEVEL_NUM_GUTTER]		= 8,
+			[LEVEL_NUM_GARBAGE]		= 2,
+			[LEVEL_NUM_BALSA]		= 5,
+			[LEVEL_NUM_PARK]		= 5.5f,
+		};
+		ObjNode* subtitle = MakeNewDriverObject(INFOBAR_SLOT, DrawLevelIntroSubtitle, 0);
+		subtitle->Timer = -timers[gLevelNum];
+	}
+
+			/* DO INTRO SCENE */
 
 	switch(gLevelNum)
 	{
